8e9d3dfae355e46873f49639cc095c615a6acd6a
ALBAS23 <maxbouxmasa3939@gmail.com>
2021-01-26 16:56:06 +0900

    posts#top ルーティング

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,9 @@ Rails.application.routes.draw do
   devise_for :users
   root to: 'posts#index'
   resources :posts, only: :index
+  resources :posts do
+    collection do
+      get 'top'
+    end
+  end 
 end
