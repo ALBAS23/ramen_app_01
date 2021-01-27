41f273c5c1d13a154e13600f01654ec93e61536b
ALBAS23 <maxbouxmasa3939@gmail.com>
2021-01-27 10:55:19 +0900

    Update#3 devise_userテーブル

--- db/schema.rb
+++ db/schema.rb
@@ -18,7 +18,7 @@ ActiveRecord::Schema.define(version: 2021_01_26_073744) do
   end
 
   create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
-    t.string "nickname", null: false
+    t.string "nickname", default: "", null: false
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
     t.integer "prefecture"
