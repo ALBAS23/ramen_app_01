7ccdf5ed2840df3d8274835d22dc02ed2e56a50c
ALBAS23 <maxbouxmasa3939@gmail.com>
2021-01-28 07:36:16 +0900

    Update#2 post.rb
    バリデーションの設定

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,19 @@
 class Post < ApplicationRecord
+  extend  ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to :genre
+  belongs_to :prefecture
+
+  with_options presence: true do 
+    validates :menu
+    validates :store
+    validates :price, format: { with: /\A[a-zA-Z0-9]+\z/ }
+    validates :genre
+    validates :word
+    validates :prefecture
+    validates :user 
+  end
+
+  validates :genre, numericality: { other_than: 1 }
+  validates :prefecture, numericality: { other_than: 1 }
+  
 end
