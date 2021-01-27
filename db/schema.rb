dd3cf8d69c9c2617f71d8c6ded5e2e8cdd18dcb8
ALBAS23 <maxbouxmasa3939@gmail.com>
2021-01-28 08:30:34 +0900

    posts table 修正

--- db/schema.rb
+++ db/schema.rb
@@ -10,11 +10,20 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_01_26_073744) do
+ActiveRecord::Schema.define(version: 2021_01_27_232314) do
 
   create_table "posts", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.string "menu", null: false
+    t.string "store", null: false
+    t.integer "price", null: false
+    t.integer "genre", null: false
+    t.string "word", null: false
+    t.integer "prefecture", null: false
+    t.string "place"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.bigint "user_id", null: false
+    t.index ["user_id"], name: "index_posts_on_user_id"
   end
 
   create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
@@ -31,4 +40,5 @@ ActiveRecord::Schema.define(version: 2021_01_26_073744) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "posts", "users"
 end
