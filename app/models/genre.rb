c79cfb6b82b971d7a5334f5b0db226c107749de9
ALBAS23 <maxbouxmasa3939@gmail.com>
2021-01-28 07:35:24 +0900

    Create genre.rb
    ActiveHashの設定

---
+++ app/models/genre.rb
@@ -0,0 +1,9 @@
+class genre < ActiveHash::Base
+  sele.data = [
+    { id: 1, name: '--' }, { id: 2, name: '味噌' }, { id: 3, name: '醤油' }, { id: 4, name: '塩' }, { id: 5, name: 'とんこつ' }, 
+    { id: 6, name: 'つけ麺' }, { id: 7, name: '油そば' }, { id: 8, name: '鶏白湯' }, { id: 9, name: '二郎系' }, { id: 10, name: 'その他' }
+  ]
+
+  include ActiveHash::Associations
+  has_many :posts
+end
