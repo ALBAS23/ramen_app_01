dcb1699a6a7f69a4ff4bf293a3ee4ac1d5a06ab3
ALBAS23 <maxbouxmasa3939@gmail.com>
2021-01-28 11:07:47 +0900

    アソシエーションとバリデーションの修正

--- app/models/post.rb
+++ app/models/post.rb
@@ -2,18 +2,17 @@ class Post < ApplicationRecord
   extend  ActiveHash::Associations::ActiveRecordExtensions
   belongs_to :genre
   belongs_to :prefecture
+  belongs_to :user
 
   with_options presence: true do 
     validates :menu
     validates :store
     validates :price, format: { with: /\A[a-zA-Z0-9]+\z/ }
-    validates :genre
     validates :word
-    validates :prefecture
-    validates :user 
+    validates :user
   end
 
-  validates :genre, numericality: { other_than: 1 }
-  validates :prefecture, numericality: { other_than: 1 }
+  validates :genre_id, numericality: { other_than: 1 }
+  validates :prefecture_id, numericality: { other_than: 1 }
   
 end
