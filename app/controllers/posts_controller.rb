77d7f36c30a896b5c137a096068947f774824498
ALBAS23 <maxbouxmasa3939@gmail.com>
2021-01-28 11:10:03 +0900

    Update post_controller.rb

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -6,11 +6,20 @@ class PostsController < ApplicationController
   end
 
   def top
-    @posts = Post.new
+
   end
 
   def new
+    @post = Post.new
+  end
 
+  def create
+    @post = Post.new(post_params)
+    if @post.save
+      redirect_to root_path
+    else
+      render :new
+    end
   end
 
   private 
@@ -19,5 +28,9 @@ class PostsController < ApplicationController
     unless user_signed_in?
       redirect_to top_posts_path 
     end
+  end
+
+  def post_params
+    params.require(:post).permit(:menu, :store, :price, :genre_id, :word, :prefecture_id, :place).merge(user_id: current_user.id)
   end
 end
