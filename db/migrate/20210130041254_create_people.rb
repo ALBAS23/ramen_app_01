dfe064c8a5c0e87cca7859971dae0f64e82a2c9b
ALBAS23 <maxbouxmasa3939@gmail.com>
2021-01-30 13:30:44 +0900

    peopleテーブル新規作成

---
+++ db/migrate/20210130041254_create_people.rb
@@ -0,0 +1,16 @@
+class CreatePeople < ActiveRecord::Migration[6.0]
+  def change
+    create_table :people do |t|
+      t.string  :gender, null: false
+      t.integer :prefecture, null: false
+      t.string  :f_store_one, null: false
+      t.string  :f_store_two
+      t.string  :f_store_three
+      t.integer :f_genre_id
+      t.string  :f_topping
+      t.text    :self_introduction
+      t.references :user, null: false, foreign_key: true
+      t.timestamps
+    end
+  end
+end
