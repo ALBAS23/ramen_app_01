baf48ddf4bc8c91aeb949dcc7b15b1bbbe67d486
ALBAS23 <maxbouxmasa3939@gmail.com>
2021-02-03 13:05:57 +0900

    バリデーション エラーメッセージ修正

--- app/models/post.rb
+++ app/models/post.rb
@@ -5,15 +5,15 @@ class Post < ApplicationRecord
   belongs_to :user
   has_one_attached  :image
 
-  with_options presence: true do 
+  with_options presence: { message: "を必ず入力してください" } do 
     validates :menu
     validates :store
-    validates :price, format: { with: /\A[a-zA-Z0-9]+\z/ }
+    validates :price, format: { with: /\A[0-9]+\z/, message: "は半角数字で入力してください" }
     validates :word
     validates :user
   end
 
-  validates :genre_id, numericality: { other_than: 1 }
-  validates :prefecture_id, numericality: { other_than: 1 }
+  validates :genre_id, numericality: { other_than: 1, message: "は必ず入力してください" }
+  validates :prefecture_id, numericality: { other_than: 1, message: "は必ず入力してください" }
   
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -5,8 +5,8 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
-  validates :nickname, presence: true, uniqueness: { case_sensitive: true }, format: { with: /\A[a-zA-Z0-9]+\z/}
-  validates :password, format: { with: /[a-z\d]{8,}/i}
+  validates :nickname, presence: true, uniqueness: { case_sensitive: true, message: "は既に登録されています" }, format: { with: /\A[a-zA-Z0-9]+\z/, message: "は半角英数字で入力してください"}
+  validates :password, format: { with: /[a-z\d]{8,}/i, message: "は半角英数字８文字以上で入力してください"}
 
   has_many :posts
   has_one :person
