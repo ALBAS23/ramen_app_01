5980a36b0c6bb6f520f73e8d9bcaa52dd2228737
ALBAS23 <maxbouxmasa3939@gmail.com>
2021-02-04 09:45:48 +0900

    devise_user.rb　バリデーション修正

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,10 +3,15 @@ class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable
+         :recoverable, :rememberable
 
-  validates :nickname, presence: true, uniqueness: { case_sensitive: true, message: "は既に登録されています" }, format: { with: /\A[a-zA-Z0-9]+\z/, message: "は半角英数字で入力してください"}
-  validates :password, format: { with: /[a-z\d]{8,}/i, message: "は半角英数字８文字以上で入力してください"}
+  with_options presence: { message: "を必ず入力してください" } do
+    validates :nickname, :email, :password, :password_confirmation
+  end
+
+  validates :nickname, uniqueness: { case_sensitive: true, message: "は既に登録されています" }, format: { with: /\A[a-zA-Z0-9]+\z/, message: "は半角英数字で入力してください"}
+  validates :email, uniqueness: { case_sensitive: true, message: "は既に登録されています"}
+  validates :password, format: { with: /[a-z\d]{8,}/i, message: "は半角英数字８文字以上で入力してください"}, confirmation: { message: "とパスワードが一致していません "}
 
   has_many :posts
   has_one :person
