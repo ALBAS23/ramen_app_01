504c6aaa762815964ee1ba17ddb855024f7dfbb5
ALBAS23 <maxbouxmasa3939@gmail.com>
2021-02-05 13:44:21 +0900

    マイページ レスポンシブ 修正

--- app/javascript/header.js
+++ app/javascript/header.js
@@ -1,12 +1,18 @@
 $(function() {
   var $modal = $('#modal-menus-show');
-  $('.fa-align-justify').on('click', function() {
+  $('.fa-th, .fa-times').on('click', function() {
     if ($modal.is(':visible')) {
       $modal.fadeOut();
       $('.container').css('display', 'block');
+      $('.profile-container').css('display', 'block');
+      $('.fa-th').fadeIn();
+      $('.fa-times').fadeOut();
     } else {
       $modal.fadeIn();
       $('.container').css('display', 'none');
+      $('.profile-container').css('display', 'none');
+      $('.fa-th').fadeOut();
+      $('.fa-times').fadeIn();
     }
   })
 })
