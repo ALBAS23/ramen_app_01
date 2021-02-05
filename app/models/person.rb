54998c79592cb83f070f54be7865e49e1e356d7d
ALBAS23 <maxbouxmasa3939@gmail.com>
2021-02-05 18:46:23 +0900

    バリデーション修正

--- app/models/person.rb
+++ app/models/person.rb
@@ -7,6 +7,7 @@ class Person < ApplicationRecord
   end
 
   validates :genre_id, numericality: { other_than: 1, message: "を必ず入力してください" }
+  validates :self_introduction, length: { maximum: 400, message: "は400文字以下で入力してください"}
 
   belongs_to :user
   belongs_to :genre
