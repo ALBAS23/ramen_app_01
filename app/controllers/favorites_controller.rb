412e7569601b6e018b03a073bf084d4f8b64633e
ALBAS23 <maxbouxmasa3939@gmail.com>
2021-02-10 18:01:25 +0900

    favoritesコントローラ編集

--- app/controllers/favorites_controller.rb
+++ app/controllers/favorites_controller.rb
@@ -1,2 +1,19 @@
 class FavoritesController < ApplicationController
+  def create
+    favorite = Favorite.new(user_id: params[:user_id], post_id: params[:post_id])
+    favorite.save
+
+    favorite = Favorite.where(user_id: params[:user_id], post_id: params[:post_id])
+
+    render json: { favorite: favorite }
+  end
+
+  def destroy
+    favorite = Favorite.find(params[:id])
+    favorite.destroy
+
+    favorite = nil;
+
+    render json: { favorite: favorite }
+  end
 end
