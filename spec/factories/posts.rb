9b866dc1e0faa2091d4b38f92d0a967624a423f7
ALBAS23 <maxbouxmasa3939@gmail.com>
2021-02-17 20:42:03 +0900

    PostモデルのFactoryBotの修正

--- spec/factories/posts.rb
+++ spec/factories/posts.rb
@@ -1,10 +1,11 @@
 FactoryBot.define do
   factory :post do
-    menu                { "チャーシュー麺" }
-    store               { "店舗" }
+    menu                { Faker::Lorem.sentence }
+    store               { Faker::Lorem.sentence }
     price               { Faker::Number.between(from: 0, to: 3000) } 
-    genre_id            { Faker::Number.between{from: 2, to: 10 } }
+    genre_id            { Faker::Number.between(from: 2, to: 10) }
     word                { Faker::Lorem.sentence } 
     prefecture_id       { Faker::Number.between(from: 2, to: 48) }
+    association :user
   end
 end
