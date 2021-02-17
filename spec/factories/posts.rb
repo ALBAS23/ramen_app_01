bfbf5c4096aedc2bfa88ea435db578404033ffe9
ALBAS23 <maxbouxmasa3939@gmail.com>
2021-02-17 20:10:57 +0900

    PostsモデルのFactoryBotを作成

--- spec/factories/posts.rb
+++ spec/factories/posts.rb
@@ -1,5 +1,10 @@
 FactoryBot.define do
   factory :post do
-    
+    menu                { "チャーシュー麺" }
+    store               { "店舗" }
+    price               { Faker::Number.between(from: 0, to: 3000) } 
+    genre_id            { Faker::Number.between{from: 2, to: 10 } }
+    word                { Faker::Lorem.sentence } 
+    prefecture_id       { Faker::Number.between(from: 2, to: 48) }
   end
 end
